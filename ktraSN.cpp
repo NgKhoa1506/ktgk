fa9c5c0d3b227acfcc6f362890ca87fc8aa7cf92
NgKhoa1506 <dh52111137@student.stu.edu.vn>
2024-11-16 09:02:00 +0700

    Add files via upload

---
+++ ktraSN.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ void main()
+{
+  int a;
+  cout<<"nhap vao so nguyen :";
+  cin>>a;
+  bool ktraChanLe(int a)
+  {
+	  if (a%2==0){
+		  cout<<"so chan";
+	  else
+		  cout<<"so le";
+	  }
+  }
+}
